83d9aed59cb77d6fa616f53fe5922cdb42e8f2ea
gnmathew <genemathewdorado@gmail.com>
2023-07-04 15:56:32 +0800

    added refactor to posts controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,48 +1,54 @@
 class PostsController < ApplicationController
-    def index
-        @posts = Post.all
-    end
 
-    def new
-        @post = Post.new
-    end 
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-    def create
-        @post = Post.new(params[:post].permit(:title, :content))
-       if @post.save
-        flash[:notice] = 'Post created successfully'
-        redirect_to posts_path
-       else
-        flash.now[:alert] = 'Post create failed'
-        render :new, status: :unprocessable_entity
-       end
-    end
+  def index
+    @posts = Post.all
+  end
 
-    def show
-        @post = Post.find(params[:id])
-    end
+  def new
+    @post = Post.new
+  end
 
-    def edit
-        @post = Post.find(params[:id])
-    end
-
-    def update
-      @post = Post.find(params[:id])
-      if @post.update(params.require(:post).permit(:title, :content))
-        flash[:notice] = 'Post updated successfully'
-        redirect_to posts_path
-      else
-        flash.now[:alert] = 'Post update failed'
-        render :edit, status: :unprocessable_entity
-       end
-    end
-    def destroy
-      @post = Post.find(params[:id])
-      @post.destroy
-      flash[:notice] = 'Post destroyed successfully'
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = 'Post created successfully'
       redirect_to posts_path
+    else
+      flash.now[:alert] = 'Post create failed'
+      render :new, status: :unprocessable_entity
     end
+  end
 
+  def show; end
 
+  def edit; end
+
+  def update
+    if @post.update(post_params)
+      flash[:notice] = 'Post updated successfully'
+      redirect_to posts_path
+    else
+      flash.now[:alert] = 'Post update failed'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:notice] = 'Post destroyed successfully'
+    redirect_to posts_path
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 
 end
