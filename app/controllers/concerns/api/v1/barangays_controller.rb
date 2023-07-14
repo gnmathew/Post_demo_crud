4486a64d9d4b7c183e5d11c30408fe022bc44513
gnmathew <genemathewdorado@gmail.com>
2023-07-14 14:41:48 +0800

    added regions city barangay provinces endpoints

---
+++ app/controllers/concerns/api/v1/barangays_controller.rb
@@ -0,0 +1,15 @@
+def index
+  city = Address::City.find_by_id(params[:city_id])
+  barangays = if city
+                city.barangays
+              else
+                Address::Barangay.all
+              end
+  render json: barangays
+end
+
+def show
+  barangay = Address::Barangay.find(params[:id])
+  render json: barangay
+end
+end
